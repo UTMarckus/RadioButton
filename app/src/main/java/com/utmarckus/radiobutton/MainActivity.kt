4f3ed9e1cf187d011f9cf2abba7a1f8080de22bb
UTMarckus <utmarckus@gmail.com>
2025-07-24 12:18:05 +0300

    feet: add functionality

--- app/src/main/java/com/utmarckus/radiobutton/MainActivity.kt
+++ app/src/main/java/com/utmarckus/radiobutton/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.utmarckus.radiobutton
 
 import android.os.Bundle
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var radioGroup: RadioGroup
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        radioGroup = findViewById(R.id.radioGroup)
+        radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            val text = when (checkedId) {
+                R.id.yesButton -> "Yes button clicked"
+                R.id.noButton -> "No button clicked"
+                R.id.maybeButton -> "Maybe button clicked"
+                else -> "Unknow button clicked"
+            }
+            Toast.makeText(this@MainActivity, text, Toast.LENGTH_SHORT).show()
+        }
     }
 }
